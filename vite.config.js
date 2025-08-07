b02f6cf6dbb9e8cff6629888fc1b9a0dcb60edd6
andriano <andrianow817@gmail.com>
2025-08-08 01:17:05 +0700

    optimized vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,42 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import viteCompression from "vite-plugin-compression";
+import viteImagemin from "vite-plugin-imagemin";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base : "/",
-  plugins: [vue(), tailwindcss()],
+  base: "/",
+  build: {
+    target: "es2015",
+    minify: "esbuild",
+    cssCodeSplit: true,
+    sourcemap:false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+    viteImagemin({
+      mozjpeg: { quality: 70 },
+      pngquant: { quality: [0.65, 0.8] },
+      svgo: {
+        plugins: [
+          { name: "removeViewBox" },
+          { name: "removeEmptyAttrs", active: false },
+        ],
+      },
+    }),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
 });
