de48843b9b166f9d995d4cbf23799651e7a2727d
andriano <andrianow817@gmail.com>
2025-08-09 14:16:00 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import viteCompression from "vite-plugin-compression";
-import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   base : "/UNIPRO-STAGE-X/",
