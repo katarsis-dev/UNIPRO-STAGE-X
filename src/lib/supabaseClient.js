a5351c22c0f1efe7c29110d5654f2b4c8aede093
zero <andrianow817@gmail.com>
2025-11-01 23:12:03 +0700

    finish test

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,9 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase_Url = import.meta.env.VITE_SUPABASE_API_URL;
 const supabase_AnonKey = import.meta.env.VITE_SUPABASE_API_ANON_KEY;
-console.log("Isi VITE_SUPABASE_URL:", import.meta.env.VITE_SUPABASE_API_URL);
-console.log(
-  "Isi VITE_SUPABASE_ANON_KEY:",
-  import.meta.env.VITE_SUPABASE_API_ANON_KEY
-);
+
 export const supabase = createClient(supabase_Url,supabase_AnonKey) 
